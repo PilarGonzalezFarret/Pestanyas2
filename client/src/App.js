ff4465c06e2373775c0388e717a23d101ae376e7
PilarGonzalezFarret <37448643+PilarGonzalezFarret@users.noreply.github.com>
2023-07-20 15:33:13 -0400

    Desarrollo

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NavTab from './components/NavTab';
+
+function App() {
+  return (
+    <div className="App">
+      <NavTab/>
+    </div>
+  );
+}
+
+export default App;
